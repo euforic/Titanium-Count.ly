5b596a6d8d1f8f113d46c9e79f57e50404873267
Christian Sullivan <cs@euforic.co>
2012-06-07 02:56:46 -0700

    first commit

---
+++ example/app.js
@@ -0,0 +1,20 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var win = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel();
+win.add(label);
+win.open();
+
+// TODO: write your module tests here
+var countly = require('count.ly');
+Ti.API.info("module is => " + countly);
+
+label.text = countly.start();
+
